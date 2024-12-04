32d447206695b34a6e2d6a9660446029ce8ba65a
jungwon <mac2001035@gmail.com>
2024-12-04 22:42:35 +0900

    FEAT: redis 키 설정 및 매출 갱신.

--- src/main/java/org/realtime/sales/service/SalesService.java
+++ src/main/java/org/realtime/sales/service/SalesService.java
@@ -1,11 +1,13 @@
 package org.realtime.sales.service;
 
 
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 @Service
@@ -19,14 +21,23 @@ public class SalesService {
     }
 
     public void updateSales(String terminalId, String paymentMethod, int amount) {
-        String key = "sales:" + terminalId;
 
-        redisTemplate.opsForHash().increment(key, paymentMethod, amount);
+        String dateKey = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMdd"));
+        String key = "dailySales:" + dateKey + ":terminal:" + terminalId + ":method:" + paymentMethod;
+
 
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime midnight = now.plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
         long secondsUntilMidnight = ChronoUnit.SECONDS.between(now, midnight);
 
-        redisTemplate.expire(key, Duration.ofSeconds(secondsUntilMidnight));
+
+        redisTemplate.execute((RedisCallback<Object>) (connection) -> {
+            byte[] redisKey = redisTemplate.getStringSerializer().serialize(key);
+
+            connection.incrBy(redisKey, amount);
+            connection.expire(redisKey, secondsUntilMidnight);
+
+            return null;
+        });
     }
 }
