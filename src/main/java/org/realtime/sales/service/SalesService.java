150d1bfdc42a391b282fc91e5e481147e58bd438
jungwon <mac2001035@gmail.com>
2024-12-05 01:51:46 +0900

    FEAT: HINCRBY로 매출 증가.

--- src/main/java/org/realtime/sales/service/SalesService.java
+++ src/main/java/org/realtime/sales/service/SalesService.java
@@ -40,4 +40,31 @@ public class SalesService {
             return null;
         });
     }
+
+    public void updateSalesWithHincrby(String terminalId, String paymentMethod, int amount) {
+        String dateKey = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMdd"));
+        String key = "dailySales:" + dateKey; // 키는 날짜 기준으로 설정
+
+        // 해당 terminalId와 paymentMethod에 대한 필드 키 생성
+        String field = "terminal:" + terminalId + ":method:" + paymentMethod;
+
+        // 자정까지의 시간 계산
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime midnight = now.plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+        long secondsUntilMidnight = ChronoUnit.SECONDS.between(now, midnight);
+
+        redisTemplate.execute((RedisCallback<Object>) (connection) -> {
+            byte[] redisKey = redisTemplate.getStringSerializer().serialize(key);
+            byte[] redisField = redisTemplate.getStringSerializer().serialize(field);
+
+            // HINCRBY로 해당 필드의 매출 값을 증가시킴
+            connection.hIncrBy(redisKey, redisField, amount);
+
+            // TTL 설정 (자정까지)
+            connection.expire(redisKey, secondsUntilMidnight);
+
+            return null;
+        });
+    }
+
 }
