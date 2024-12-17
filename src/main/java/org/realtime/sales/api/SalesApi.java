4de3b63a006fb2bbff7fbce48852a7f7b03f117a
jungwon <mac2001035@gmail.com>
2024-12-18 00:58:53 +0900

    오늘 매출 결제 수단별로 구하기

---
+++ src/main/java/org/realtime/sales/api/SalesApi.java
@@ -0,0 +1,30 @@
+package org.realtime.sales.api;
+
+import lombok.RequiredArgsConstructor;
+import org.realtime.sales.service.StoreSalesService;
+import org.realtime.sales.service.dto.TodaySales;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class SalesApi {
+
+    private final StoreSalesService storeSalesService;
+
+    @GetMapping("/sale/store/{storeId}")
+    public List<TodaySales> getStoreSales(@PathVariable("storeId") Long storeId){
+        List<String> terminalLists = getTerminalLists(storeId);
+
+        List<TodaySales> todaySalesList = storeSalesService.calculateTodaySalesByPaymentMethod(terminalLists);
+
+        return todaySalesList;
+    }
+
+    private List<String> getTerminalLists(Long storeId) {
+        return List.of("12345667","12345668","12345669");
+    }
+}
