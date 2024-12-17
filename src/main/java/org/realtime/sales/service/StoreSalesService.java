2e74aba4ef3e302b500eb606c954398b06a41cc9
jungwon <mac2001035@gmail.com>
2024-12-17 23:46:27 +0900

    FEAT: 결제수단별 매출 구하기.

--- src/main/java/org/realtime/sales/service/StoreSalesService.java
+++ src/main/java/org/realtime/sales/service/StoreSalesService.java
@@ -14,9 +14,26 @@ public class StoreSalesService {
         this.salesManageService = salesManageService;
     }
 
-    public TodaySales getTodayTotalSales(List<String> terminalIds,PaymentMethod paymentMethod){
-        Integer todaySales = 0;
+    public List<TodaySales> calculateTodaySalesByPaymentMethod(List<String> terminalIds){
 
+        List<TodaySales> todaySalesList = new ArrayList<>();
+
+        List<PaymentMethod> paymentMethods = new ArrayList<>();
+
+        paymentMethods.add(PaymentMethod.CARD);
+        paymentMethods.add(PaymentMethod.POINT);
+        paymentMethods.add(PaymentMethod.CASH);
+
+        for(PaymentMethod paymentMethod : paymentMethods){
+            todaySalesList.add(getTodayTotalSales(terminalIds,paymentMethod));
+        }
+
+        return todaySalesList;
+    }
+
+
+    private TodaySales getTodayTotalSales(List<String> terminalIds,PaymentMethod paymentMethod){
+        Integer todaySales = 0;
 
         for (String terminalId : terminalIds) {
             todaySales += salesManageService.getSalesValue(terminalId, paymentMethod);
